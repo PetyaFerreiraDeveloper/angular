4b70fb5739e7fda57e630719f4b2cfe84b51b6db
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2022-11-12 10:25:02 +0100

    start up the forum project

--- 02-week/workshop/rest-api/config/config.js
+++ 02-week/workshop/rest-api/config/config.js
@@ -1,9 +1,12 @@
 const env = process.env.NODE_ENV || 'development';
+const url = process.env.MONGODB_ATLAS_URL;
 
 const config = {
     development: {
         port: process.env.PORT || 3000,
         dbURL: 'mongodb://localhost:27017/forum',
+        // dbURL: 'mongodb+srv://test1:test123@cluster0.qfegruh.mongodb.net/?retryWrites=true&w=majority',
+        // dbURL: url,
         origin: ['http://localhost:5555', 'http://localhost:4200']
     },
     production: {
