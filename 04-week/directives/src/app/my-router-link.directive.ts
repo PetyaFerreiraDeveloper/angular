de38dc627a9e7bede43efb725a6e1d77863c9a54
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2022-11-25 09:28:58 +0100

    create an attribute directive

---
+++ 04-week/directives/src/app/my-router-link.directive.ts
@@ -0,0 +1,35 @@
+import { Directive, ElementRef, OnDestroy, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appMyRouterLink]',
+})
+export class MyRouterLinkDirective implements OnDestroy {
+  unsubs: (() => void)[] = [];
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+    this.unsubs.push(this.renderer.listen(
+      this.elementRef.nativeElement,
+      'mouseover',
+      this.mouseOverHandler
+    ));
+    this.unsubs.push(this.renderer.listen(
+    this.elementRef.nativeElement,
+      'mouseleave',
+      this.mouseLeaveHandler
+    ));
+   
+    // this.renderer.setAttribute(this.elementRef.nativeElement, 'data-test', '123');
+  }
+
+  mouseOverHandler = (e: MouseEvent) => {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'red');
+  };
+
+  mouseLeaveHandler = (e: MouseEvent) => {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'initial');
+  }
+
+  ngOnDestroy(): void {
+    this.unsubs.forEach(fn => fn());
+  }
+}
